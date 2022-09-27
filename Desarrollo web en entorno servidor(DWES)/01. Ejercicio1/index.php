0ca207c4c4cc9dc3eaba630ac1a7fb6af1539bf2
GallardoAlberto <gallardo.alb.1998@gmail.com>
2022-09-27 16:35:49 +0200

    Ejercicio Array Asociativa PHP

---
+++ Desarrollo web en entorno servidor(DWES)/01. Ejercicio1/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio ARRAY ASOCIATIVO</title>
+</head>
+<body>
+    <?php
+        $nombre_peso = array(
+         "Alberto" => 85,
+         "Rulo" => 70,
+         "Victor" => 60,
+         "Moleiro" => 80,
+         "Gustavo" => 85,
+         "Alex" => 50,
+         "Sebastian" => 70
+        );
+
+         print "<h1>" . $nombre_peso . "</h1>";
+    ?>
+</body>
+</html>
